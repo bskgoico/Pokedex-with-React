228c48c21bf4571058b091dbfa2c587ac606b599
ufobsk <igoico10@gmail.com>
2023-07-22 18:24:47 -0300

    first-commit

--- src/api.js
+++ src/api.js
@@ -7,14 +7,14 @@ export const searchPokemon = async (pokemon) => {
   } catch (err) {}
 };
 
-export const getPokemons = async(limit=10, offset=0) => {
+export const getPokemons = async (limit = 25, offset = 0) => {
   try {
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset${offset}`;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
     const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (err) {}
-}
+};
 
 export const getPokemonData = async (url) => {
   try {
@@ -22,4 +22,4 @@ export const getPokemonData = async (url) => {
     const data = await response.json();
     return data;
   } catch (err) {}
-}
+};

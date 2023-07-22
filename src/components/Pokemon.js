28b0082e36661f9e1178f285e27bb1bb5880739e
ufobsk <igoico10@gmail.com>
2023-07-22 18:16:22 -0300

    first-commit

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,12 +1,17 @@
 const Pokemon = (props) => {
   const { pokemon } = props;
+  console.log(pokemon);
   return (
     <div className="pokemon-card">
-      <div className="pokemon.img">
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <div className="pokemon.img-container">
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          className="pokemon-img"
+        />
       </div>
       <div>
-        <div>
+        <div className="card-top">
           <h3>{pokemon.name}</h3>
           <div>#{pokemon.id}</div>
         </div>

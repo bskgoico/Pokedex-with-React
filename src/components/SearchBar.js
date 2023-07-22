8fcdfac7eef8703d596e3eca4a2b1d9b2063232b
ufobsk <igoico10@gmail.com>
2023-07-22 16:52:15 -0300

    first-commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,29 +2,31 @@ import { useState } from "react";
 import { searchPokemon } from "../api";
 
 const SearchBar = () => {
-    
-    const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
+  const [pokemon, setPokemon] = useState();
 
-    const onChange = (evt) => {
-        console.log(evt.target.value);
-        setSearch(evt.taget.value);
-    }
+  const onChange = (evt) => {
+    setSearch(evt.target.value);
+  };
 
-    const onClick = async (evt) => {
-        const data = await searchPokemon(search);
-        console.log(data);
-    }
+  const onClick = async (evt) => {
+    const data = await searchPokemon(search);
+    setPokemon(data);
+  };
 
-    return (
-        <div>
-            <div>
-                <input placeholder="Buscar Pokemon..." onChange={onChange} />
-            </div>
-            <div>
-                <button onClick={onClick}>Buscar</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="searchbar-container">
+      <div className="searchbar">
+        <input placeholder="Buscar Pokemon..." onChange={onChange} />
+      </div>
+      <div className="searchbar-btn">
+        <button onClick={onClick}>Buscar</button>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
 
 export default SearchBar;
